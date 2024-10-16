ab8ac261b8b2d00a145ab3074334dae65101cee4
Irina Bubela <bubelairene@gmail.com>
2024-10-16 16:04:48 +0200

    routing - finished

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,12 @@
+export default function PrivateRoute() {
+    return <h1>You cannot view this in the browser</h1>
+}
+
+//_lib - makes component invisible for routing
+//it creates private folder
+
+// => we can separate logic from UI
+// => organizing internal files
+// => avoiding potential naming conflicts
+
+//you want to have __ in your URL? -put %5F
