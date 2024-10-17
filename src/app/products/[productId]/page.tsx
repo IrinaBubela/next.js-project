d1e7903b7ddec5cc7cbdee101377c2253b70209c
Irina Bubela <bubelairene@gmail.com>
2024-10-17 23:38:36 +0200

    changing route structure and adding metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,18 @@
-export default function ProductList({
-    params,
-}: {
-    params: { productId: string };
-}) {
+import {Metadata} from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+
+export const generateMetadata = ({params}: Props): Metadata => {
+    return {
+        title: `Product ${params.productId}`
+    }
+}
+
+export default function ProductDetails({params}: Props) {
     return (
         <>
             <h1>Details about product {params.productId}</h1>
